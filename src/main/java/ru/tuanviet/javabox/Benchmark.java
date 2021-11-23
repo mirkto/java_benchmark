b486d0376525922681b7c7f60aef97557732d7bf
mirkto <thehisev@mail.ru>
2021-11-23 17:01:40 +0300

    first comm 2

--- src/main/java/ru/tuanviet/javabox/Benchmark.java
+++ src/main/java/ru/tuanviet/javabox/Benchmark.java
@@ -1,5 +1,9 @@
 package ru.tuanviet.javabox;
 
+import java.lang.annotation.*;
+
+@Target(value= ElementType.METHOD)
+@Retention(value= RetentionPolicy.RUNTIME)
 public @interface Benchmark {
 
     int repeats();
